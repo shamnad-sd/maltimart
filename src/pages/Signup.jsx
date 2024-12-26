175b0336b618a680debe4844880269037b579b9b
SdCode000 <shamnad2nk@gmail.com>
2024-12-26 22:30:21 +0530

    maltimart

--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -66,7 +66,7 @@ const Signup = () => {
               <Col lg='12' className='text-center'><h5 className='font-bold'>Loading.....</h5></Col>
             ) : (
               <Col lg='6' className='m-auto text-center'>
-                <h3 className="font-bold fs-4 mb-4">Signup</h3>
+                <h3 className="font-bold fs-4 mb-4">SIGNUP</h3>
 
 
                 <Form className='auth__form' onSubmit={signup}>
